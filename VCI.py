a70bea61b56737f2b31dda978bb3c22dafa1702e
shenbagabalan <46273794+shenbagabalan@users.noreply.github.com>
2019-05-15 09:02:52 +0530

    Create VCI.py

---
+++ VCI.py
@@ -0,0 +1,8 @@
+character=input()
+if character.isalpha() == True:
+    if(character=='A' or character=='a' or character=='E' or character =='e' or character=='I'or character=='i' or character=='O' or character=='o' or character=='U' or character=='u'):
+        print("Vowel")
+    else:
+        print("consonant")
+else:
+    print("Invalid")
