13558fbe93af9de48d9dde1dd0459de159391240
shenbagabalan <46273794+shenbagabalan@users.noreply.github.com>
2019-05-15 08:26:12 +0530

    Update oddreven.py

--- oddreven.py
+++ oddreven.py
@@ -1,5 +1,9 @@
-number=int(input())
-if(number%2==0):
-    print("Even")
-else:
-    print("Odd")
+number=input()
+try:
+    number=int(number)
+    if(number%2==0):
+        print("Even")
+    else:
+        print("Odd")
+except ValueError:
+        print("Invalid")
