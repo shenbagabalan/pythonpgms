ab8883b1bd597659897487d7e73d9c8cbec3d468
shenbagabalan <46273794+shenbagabalan@users.noreply.github.com>
2019-05-15 09:04:09 +0530

    Update VCI.py

--- VCI.py
+++ VCI.py
@@ -3,6 +3,6 @@ if character.isalpha() == True:
     if(character=='A' or character=='a' or character=='E' or character =='e' or character=='I'or character=='i' or character=='O' or character=='o' or character=='U' or character=='u'):
         print("Vowel")
     else:
-        print("consonant")
+        print("Consonant")
 else:
     print("Invalid")
