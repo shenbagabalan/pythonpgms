ec252985b47e5aa53fed99fcab571c065fb8167d
shenbagabalan <46273794+shenbagabalan@users.noreply.github.com>
2019-06-20 17:55:29 +0530

    Create Rom.py

---
+++ Rom.py
@@ -0,0 +1,5 @@
+sh=input()
+se=["0","I","II","III","IV","V","VI","VII","VIII","IX","X","XI","XII","XIII","XIV","XV","XVI","XVII","XVIII","XIX","XX"]
+for i in range(21):
+  if(sh==se[i]):
+    print(i)
