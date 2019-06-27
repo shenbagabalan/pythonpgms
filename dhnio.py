b9fb5e3bfc871675ea69496c833be436a7d9d386
shenbagabalan <46273794+shenbagabalan@users.noreply.github.com>
2019-06-27 09:37:15 +0530

    Update and rename dhnio.py to dho.py

--- dhnio.py
+++
@@ -1,6 +0,0 @@
-from collections import Counter 
-st=input()
-if(Counter(st)==Counter("dhoni")):
-    print("yes",end="")
-else:
-    print("no",end="")
---
+++ dho.py
@@ -0,0 +1,6 @@
+from collections import Cr 
+st=input()
+if(Cr(st)==Cr("dhoni")):
+    print("yes",end="")
+else:
+    print("no",end="")
