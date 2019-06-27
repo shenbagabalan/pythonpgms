d609b4f3e61359b76596cadb84e369c745521db9
shenbagabalan <46273794+shenbagabalan@users.noreply.github.com>
2019-06-27 08:00:46 +0530

    Update and rename Arri.py to Arrin.py

--- Arrin.py
+++ Arrin.py
@@ -8,8 +8,8 @@ for i in range(0,sq):
     u,v=map(int,input().split())
     for j in range(u-1 ,v):
         b3.append(a3[j])
-    x=sum(b3)
-    y3.append(x)
+    sx=sum(b3)
+    y3.append(sx)
 print(y3[0])
 for z in range(1,len(y3)):
-    print(y3[z]- y3[z- 1])Arr
+    print(y3[z]- y3[z- 1])
